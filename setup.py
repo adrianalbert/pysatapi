c460b44db7c356f4445afb9d89f12e6251bf1064
Adrian Albert <adalbert@ocio-gpu01.slac.stanford.edu>
2017-07-30 10:29:12 -0700

    preparing package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='satapi',
+      version='0.1',
+      description='Python wrapper around several satellite imagery APIs',
+      url='https://github.com/adrianalbert/satapi',
+      author='Adrian Albert',
+      author_email='adrian.t.albert@gmail.com',
+      license='MIT',
+      packages=['satapi'],
+      zip_safe=False)
